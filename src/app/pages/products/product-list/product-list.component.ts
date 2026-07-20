eb5866a903af6e97d676b1c15ce19978a934756e
Seby <lokuraa18@hotmail.com>
2026-07-20 01:43:20 -0300

    estructura minima

--- src/app/pages/products/product-list/product-list.component.ts
+++ src/app/pages/products/product-list/product-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+
+import { ProductService } from '../../../services/product.service';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-product-list',
-  imports: [RouterLink],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {}
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+  loading = true;
+  error = '';
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAll().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Error al cargar productos';
+        this.loading = false;
+      }
+    });
+  }
+}
